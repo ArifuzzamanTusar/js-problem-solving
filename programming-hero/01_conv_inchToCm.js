0471cafe70c19eea480d8e82d9ea60d2ec462934
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-02-06 23:48:02 +0600

    3

---
+++ programming-hero/01_conv_inchToCm.js
@@ -0,0 +1,5 @@
+function inchToCm(inch) {
+    let cm = inch*2.54;
+    return cm;
+}
+console.log(inchToCm(500));
---
+++ programming-hero/02_conv_mileToKm.js
@@ -0,0 +1,5 @@
+function mileToKm(mile) {
+    let km = mile*1.60934;
+    return km;
+}
+console.log(mileToKm(500));
